22082e652d8c37a37343a9da8dc98edc05bd77ba
Christopher Clark <chris@lettersfromtaiwan.com>
2013-08-02 21:54:15 +0000

    Added index.html

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    Buffer b = fs.readFileSync('index.html');
+    response.send(b.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
