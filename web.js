4dfaa52c8f3e0dbbabebc8a1e740f508fcf2e51c
Christopher Clark <chris@lettersfromtaiwan.com>
2013-08-02 22:08:43 +0000

    Fixed index.html

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
     Buffer b = fs.readFileSync('index.html');
